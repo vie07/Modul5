b8d5e455a298cbf741df6c853aee06df69ba1dbe
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2026-04-01 07:31:48 +0700

    menambahkan class PemrosesData dengan Method DapatkanNilaiTerbesar

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class PemrosesData
+{
+    public T DapatkanNilaiTerbesar<T>(T a, T b, T c)
+    {
+        dynamic temp1 = a;
+        dynamic temp2 = b;
+        dynamic temp3 = c;
+
+        dynamic max = a;
+        if (temp2 > max)
+        {
+            max = temp2;
+        } 
+        if (temp3 > max)
+        {
+            max = temp3;
+        }
+
+        return max;
+    }
+}
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        PemrosesData nim = new PemrosesData();
+        int terbesar = nim.DapatkanNilaiTerbesar(10, 30, 22);
+        Console.WriteLine("Nilai terbesar: " + terbesar);
+    }
+}
